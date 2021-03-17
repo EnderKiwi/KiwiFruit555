b31026e4cc7eea8aede0acba02c720fe5e20aef7
KiwiFruit555 <64765206+KiwiFruit555@users.noreply.github.com>
2021-03-17 13:12:13 -0400

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,30 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: amatsuki555
+ *
+ * Created on March 16, 2021, 9:16 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout<<"Enter your name: ";
+    string name;
+    cin>>name;
+    cout<<"Hello, "<<name<<"!";
+    
+    return 0;
+}
